195ca4144faab2d556601487811631f61a1a0cb9
ziad yousef <ziadatef35@hotmail.com>
2020-12-30 14:13:33 +0200

    Home

---
+++ Front/App.vue
@@ -0,0 +1,22 @@
+<template>
+<Home/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
