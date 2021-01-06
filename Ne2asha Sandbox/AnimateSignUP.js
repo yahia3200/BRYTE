281b04b6153b10b0f8f696c28c9310c19ef43dc3
ziad yousef <ziadatef35@hotmail.com>
2021-01-06 12:21:34 +0200

    sign up divided

--- Ne2asha Sandbox/AnimateSignUP.js
+++ Ne2asha Sandbox/AnimateSignUP.js
@@ -5,17 +5,13 @@ document.querySelector("#sign-up-dev").addEventListener("click", ()=> {
     gsap.to(`#${event.target.id}`, { duration: 0.1, y: 20, opacity: 0 });
     gsap.to(".Cont-Block.dev", { delay: 0.5, duration: 1, x: 840, y: 40 });
     gsap.to(".signUP-form-Cont-1", { delay: 1, duration: 0.4, y: -80});
-    gsap.to(".signUP-form-Cont-1 .Input-Cont, #Continue-dev", { delay: 1, duration: 0.4, y:-80, opacity: 1, stagger: 0.1});
+    gsap.to(".signUP-form-Cont-1 .Input-Cont, #Complete-dev", { delay: 1, duration: 0.4, y:-80, opacity: 1, stagger: 0.1});
     gsap.to(".UnderLineTitle", { delay: 1, duration: 0.3, x:80, opacity: 1});
     document.querySelector(".signUP-form-Cont-1").style.pointerEvents = 'all';
   });
-  document.querySelector("#Continue-dev").addEventListener("click", ()=> {
-    gsap.to(".signUP-form-Cont-1", { delay: 0, duration: 0.4, y: 80});
-    gsap.to(".signUP-form-Cont-1 .Input-Cont, #Continue-dev", { delay: 0, duration: 0.4, y:80, opacity: 0});
-    document.querySelector(".signUP-form-Cont-1").style.pointerEvents = 'none';
-    gsap.to(".signUP-form-Cont-2", { delay: 0.5, duration: 0.4, y: -80});
-    gsap.to(".signUP-form-Cont-2 .Input-Cont, #Complete-dev", { delay: 0.4, duration: 0.4, y:-80, opacity: 1, stagger: 0.1});
-    document.querySelector(".signUP-form-Cont-2").style.pointerEvents = 'all';
-    gsap.to("circle", { duration: 0.5, fill: '#4B90F7'});
-    document.querySelector(".TitleAbove").innerHTML = "Personal Info";
-  });
+document.querySelector("#form").addEventListener("submit", (e)=>{
+  e.preventDefault();
+  gsap.to(".signUP-form-Cont-1", { delay: 0, duration: 0.4, y: 80});
+  gsap.to(".signUP-form-Cont-1 .Input-Cont, #Continue-dev", { delay: 0, duration: 0.4, y:80, opacity: 0});
+  document.querySelector(".signUP-form-Cont-1").style.pointerEvents = 'none';
+});
