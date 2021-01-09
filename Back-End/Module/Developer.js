852744547996afcaa6e97bf188d16a1537406361
yahia3200 <yahiazakaria3200@gmail.com>
2021-01-09 15:23:46 +0200

    dev

--- Back-End/Module/Developer.js
+++ Back-End/Module/Developer.js
@@ -2,12 +2,78 @@ const { pool } = require('./DBConfig');
 
 const getPortofiloInfo = async (id)=>{
     res = {};
+
+    // Get Developers Info
     let query = "SELECT * FROM developer WHERE DEV_ID = ?";
-    const [rows, fields] = await pool.promise().query(query, [id]);
+    let [rows] = await pool.promise().query(query, [id]);
     res['dev'] = Object.assign({}, rows[0]);
-    query = "SELECT * "; 
+
+    // Get Projects that the developer worked on
+    query = "SELECT * FROM developer_works_on WHERE DEV_ID = ?";
+    [rows] = await pool.promise().query(query, [id]);
+    rows = rows.map(v => Object.assign({}, v));
+
+    // Get Projects Info and Categories
+    res['projects'] = [];
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        
+         // get project info
+         query = "SELECT * FROM PROJECT WHERE PRO_ID = ?";
+         let [info] = await pool.promise().query(query, [row['PRO_Id']]);
+         info = Object.assign({}, info[0]);
+ 
+         // get project fileds
+         query = "SELECT DISTINCT PRO_CAT_Field FROM pro_category WHERE CAT_PRO_ID = ?";
+         let [fileds] = await pool.promise().query(query, [row['PRO_Id']]);
+         fileds = fileds.map(v => Object.assign({}, v));
+         fileds = fileds.map(v => v['PRO_CAT_Field']);
+         info['fileds'] = fileds;
+
+         // get project skills
+         query = "SELECT DISTINCT PRO_CAT_Skill FROM pro_category WHERE CAT_PRO_ID = ?";
+         let [skills] = await pool.promise().query(query, [row['PRO_Id']]);
+         skills = skills.map(v => Object.assign({}, v));
+         skills = skills.map(v => v['PRO_CAT_Skill']);
+         info['skills'] = skills;
+ 
+         res['projects'].push(info);
+    }
+
+
+    // Get Developers Categories and Skills
+    query = "SELECT DISTINCT DEV_CAT_Field FROM dev_category WHERE CAT_Developer_Id = ?";
+    let [fileds] = await pool.promise().query(query, [id]);
+    fileds = fileds.map(v => Object.assign({}, v));
+    fileds = fileds.map(v => v['DEV_CAT_Field']);
+    res['fileds'] =  fileds;
+
+    query = "SELECT DISTINCT DEV_CAT_Skill FROM dev_category WHERE CAT_Developer_Id = ?";
+    let [skills] = await pool.promise().query(query, [id]);
+    skills = skills.map(v => Object.assign({}, v));
+    skills = skills.map(v => v['DEV_CAT_Skill']);
+    res['skills'] =  skills;
+    
+    // Get developer experince 
+    query = "SELECT * FROM dev_working_experince WHERE DEV_WOR_Developer_Id = ?";
+    let [experince] = await pool.promise().query(query, [id]);
+    experince = experince.map(v => Object.assign({}, v));
+    res['experince'] = experince;
+
+    // Get developer degrees 
+    query = "SELECT * FROM dev_degree WHERE DEV_DEG_Developer_Id = ?";
+    let [degrees] = await pool.promise().query(query, [id]);
+    degrees = degrees.map(v => Object.assign({}, v));
+    res['degrees'] = degrees;
+
+    // Get developer links 
+    query = "SELECT * FROM dev_links WHERE DEV_LIN_Developer_Id = ?";
+    let [links] = await pool.promise().query(query, [id]);
+    links = links.map(v => Object.assign({}, v));
+    res['links'] = links;
+
     console.log(res);
-    return Object.assign({}, rows[0]);
+    return res;
 
 }
 
---
+++ DB/script.sql
@@ -0,0 +1,213 @@
+USE BRYTE;
+
+/* Insert Developers*/
+INSERT INTO developer (
+    DEV_Fname,
+    DEV_Lname,
+    DEV_User_Name,
+    DEV_Email,
+    DEV_Creation_Date,
+    DEV_Birth_Date,
+    DEV_Last_Login,
+    DEV_Hash,
+    DEV_Credentials,
+    DEV_Wallet,
+    DEV_Wining_Count,
+    DEV_Participation_Count,
+	DEV_Pick_Rate,
+	DEV_Bids_Count,
+    DEV_Projects_Count,
+    DEV_Age,
+    DEV_Address,
+    DEV_Job_Title,
+    DEV_Phone
+     )
+    VALUES (
+    "Yahia",
+    "Zakaria",
+    "yahia3200",
+    "yahia@gmail.com",
+    "2020-02-02",
+    "2000-03-07",
+    "2020-02-02",
+    '$2b$10$2FM5RH0M5KiCgjx5pO3CYu.v1JR.AN8UupehuUX7l.SiyRlibb32i',
+    "Trial",
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    20,
+    "Haram Street",
+    "Computer Engineer",
+	"01023071750"
+    );
+
+INSERT INTO developer (
+    DEV_Fname,
+    DEV_Lname,
+    DEV_User_Name,
+    DEV_Email,
+    DEV_Creation_Date,
+    DEV_Birth_Date,
+    DEV_Last_Login,
+    DEV_Hash,
+    DEV_Credentials,
+    DEV_Wallet,
+    DEV_Wining_Count,
+    DEV_Participation_Count,
+	DEV_Pick_Rate,
+	DEV_Bids_Count,
+    DEV_Projects_Count,
+    DEV_Age,
+    DEV_Address,
+    DEV_Job_Title,
+    DEV_Phone
+     )
+    VALUES (
+    "Hossam",
+    "Saeed",
+    "Hossam3200",
+    "Hosam@gmail.com",
+    "2020-03-04",
+    "2001-04-04",
+    "2020-03-04",
+    '$2b$10$2FM5RH0M5KiCgjx5pO3CYu.v1JR.AN8UupehuUX7l.SiyRlibb32i',
+    "Trial",
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    19,
+    "Fisal Street",
+    "Computer Graphics Engineer",
+	"01222051750"
+    );
+
+INSERT INTO developer (
+    DEV_Fname,
+    DEV_Lname,
+    DEV_User_Name,
+    DEV_Email,
+    DEV_Creation_Date,
+    DEV_Birth_Date,
+    DEV_Last_Login,
+    DEV_Hash,
+    DEV_Credentials,
+    DEV_Wallet,
+    DEV_Wining_Count,
+    DEV_Participation_Count,
+	DEV_Pick_Rate,
+	DEV_Bids_Count,
+    DEV_Projects_Count,
+    DEV_Age,
+    DEV_Address,
+    DEV_Job_Title,
+    DEV_Phone
+     )
+    VALUES (
+    "Ossama",
+    "Magdy",
+    "Ossama3200",
+    "Ossama@gmail.com",
+    "2021-01-01",
+    "2000-02-01",
+    "2021-01-01",
+    '$2b$10$2FM5RH0M5KiCgjx5pO3CYu.v1JR.AN8UupehuUX7l.SiyRlibb32i',
+    "Trial",
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    20,
+    "Fisal Street",
+    "Softwear Engineer",
+	"01021021750"
+    );
+
+INSERT INTO developer (
+    DEV_Fname,
+    DEV_Lname,
+    DEV_User_Name,
+    DEV_Email,
+    DEV_Creation_Date,
+    DEV_Birth_Date,
+    DEV_Last_Login,
+    DEV_Hash,
+    DEV_Credentials,
+    DEV_Wallet,
+    DEV_Wining_Count,
+    DEV_Participation_Count,
+	DEV_Pick_Rate,
+	DEV_Bids_Count,
+    DEV_Projects_Count,
+    DEV_Age,
+    DEV_Address,
+    DEV_Job_Title,
+    DEV_Phone
+     )
+    VALUES (
+    "Ziad",
+    "Youssef",
+    "Ziad3200",
+    "Ziad@gmail.com",
+    "2020-05-01",
+    "2000-02-01",
+    "2021-01-01",
+    '$2b$10$2FM5RH0M5KiCgjx5pO3CYu.v1JR.AN8UupehuUX7l.SiyRlibb32i',
+    "Trial",
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    20,
+    "ABBAS Street",
+    "Front-End Developer",
+	"01021021750"
+    );
+/* Insert Projects */
+
+INSERT INTO project (
+    PRO_Name,
+    PRO_Start_Date,
+    PRO_Creation,
+    PRO_End_Date,
+    PRO_Description,
+    PRO_Rate,
+    PRO_Thumbnail
+  )
+    VALUES (
+    'C.A.T.S',
+    '2020-12-26',
+    '2020-12-26',
+    '2020-12-26',
+    'A GREAT Game with great name',
+    5,
+    'https://www.catsthegame.com/assets/og-ba95d791dacf713de66d6d4c955a76b05141352b4d3a7ceda252cb23197d1805.png'
+  );
+
+INSERT INTO project (
+    PRO_Name,
+    PRO_Start_Date,
+    PRO_Creation,
+    PRO_End_Date,
+    PRO_Description,
+    PRO_Rate,
+    PRO_Thumbnail
+  )
+    VALUES (
+    'C.A.T.S',
+    '2020-12-26',
+    '2020-12-26',
+    '2020-12-26',
+    'A GREAT Game with great name',
+    5,
+    'https://www.catsthegame.com/assets/og-ba95d791dacf713de66d6d4c955a76b05141352b4d3a7ceda252cb23197d1805.png'
+  );
