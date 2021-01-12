715973c6caf267c7952d31f4c10f0f3e3f0d3000
yahia3200 <yahiazakaria3200@gmail.com>
2021-01-12 14:47:46 +0200

    client change pass

--- Back-End/Routes/devRoutes.js
+++ Back-End/Routes/devRoutes.js
@@ -1,4 +1,5 @@
 const Router = require('express');
+const { isDev } = require("../middleware/authmiddleware");
 
 const controller = require("../Controller/devController");
 
@@ -7,7 +8,7 @@ const router = Router();
 router.get("/developer/:id", controller.developerInfo);
 router.post("/check_new_user_name", controller.checkNewUser);
 router.post('/check_new_email', controller.checkEmail);
-router.get('/change-password', (req, res)=>{res.render('ChangePass', {style : "Login"})});
+router.get('/change-password', isDev, (req, res)=>{res.render('ChangePass', {style : "Login"})});
 router.post('/change-password', controller.changePassword);
 
 module.exports = router;
